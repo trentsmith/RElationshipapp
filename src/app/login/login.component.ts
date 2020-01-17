c76bb6df9ff6a3f41aab374c937c8bddd3197469
trentsmith <smithtrent00@gmail.com>
2020-01-16 23:46:52 -0500

    1 file modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit
 
     let json = {username:"username3",password:"password3"};
 
-      this.getData("localhost:3000/user");
+      this.getData("http://www.mocky.io/v2/5e213a252f0000670077d4d4");
 
       this.value=0;
 
@@ -47,141 +47,71 @@ export class LoginComponent implements OnInit
   getData(url:string)
 
   {
-
     this.http.get(url)
-
     .subscribe(
-
       (data:any[])=>
-
       {
-
           this.data=data;
-
-          console.log(this.data);
-
       }
-
     ) 
-
   }
 
   showReg:boolean=false;
-
   register()
-
   {
-
     this.showReg=!this.showReg;
-
   }
-
   updateData(url:string)
-
   {
-
     let json = 
-
     {
-
       id:100,
-
       username:"username2",
-
       password:"password2"
-
     };
-
     this.http.put(url,json).subscribe(
-
       (data:any[])=>
-
       {
-
       }
-
     );
-
   }
-
   postData(url:string,json:any)
-
   {
-
     this.http.post(url, json).subscribe(
-
       (data: any[]) => 
-
       {
-
       }
-
     );
-
     console.log(json);
-
   }
-
   changeValue(n:number)
-
   {
-
     this.value=this.value+n;
-
   }
-
   name:string;
-
   onNameKeyUp(event:any)
-
   {
-
     this.name =event.target.value;
-
   }
-
   onPasswordKeyUp(event:any)
-
   {
-
     this.password =event.target.value;
-
   }
-
   showInfo1:boolean=false;
-
-
-
   getUserbyUsername()
-
   {
-
     var i =0;
-
     for(i=0;i<this.data.length;i++)
-
     {
-
       if(this.data[i].username==this.name)
-
       {
-
         if(this.data[i].password==this.password)
-
         {
-
           this.username=this.name;
-
           this.showInfo1=!this.showInfo1;
-
           console.log(this.username);
-
         }
-
       }
-
     }
-
   }
-
 }
